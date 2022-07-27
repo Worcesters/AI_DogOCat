6ba9f37c332f806537c4e42147b585fad10714b7
Worcesters <jeremy.didier77@gmail.com>
2022-07-27 14:51:39 +0200

    mise a jour librairy

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,6 +1,6 @@
 import * as PIXI from "https://cdn.skypack.dev/pixi.js";
 import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur";
-import SimplexNoise from "https://cdn.skypack.dev/simplex-noise";
+import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
 import debounce from "https://cdn.skypack.dev/debounce";
 
