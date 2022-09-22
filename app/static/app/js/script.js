96ccf0d5cce48238c71e09db797211d45cc2a266
Worcesters <jeremy.didier77@gmail.com>
2022-09-22 16:37:42 +0200

    last update

--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,8 +1,8 @@
-import * as PIXI from "https://cdn.skypack.dev/pixi.js";
-import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur";
-import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3";
-import hsl from "https://cdn.skypack.dev/hsl-to-hex";
-import debounce from "https://cdn.skypack.dev/debounce";
+import * as PIXI from "https://cdn.skypack.dev/pixi.js@6.5.3";
+import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur@4.2.0";
+import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.1";
+import hsl from "https://cdn.skypack.dev/hsl-to-hex@1.0.0";
+import debounce from "https://cdn.skypack.dev/debounce@1.2.1";
 
 // return a random number within a range
 function random(min, max) {
